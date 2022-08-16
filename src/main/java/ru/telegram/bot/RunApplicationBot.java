5853b5249d457ddabbc575c7ebf9077957b2b81c
abeglyarov <abeglyarov@axbit.ru>
2022-08-16 17:53:02 +0400

    Создание кракаса

---
+++ src/main/java/ru/telegram/bot/RunApplicationBot.java
@@ -0,0 +1,11 @@
+package ru.telegram.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunApplicationBot {
+    public static void main(String[] args) {
+        SpringApplication.run(RunApplicationBot.class, args);
+    }
+}
